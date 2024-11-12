ccd41e0f408588b095b24a0aeba242c57481fec2
Laptop Home <business.mahtabnihar@gmail.com>
2024-11-12 23:56:09 +0600

    updated design and added demo leaderboard.dart

--- my_app/lib/App/View/Widgets/LeaderBoardUserPosition.dart
+++ my_app/lib/App/View/Widgets/LeaderBoardUserPosition.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 
 class Leaderboarduserposition extends StatelessWidget {
   final userRank = 99999;
--- my_app/lib/App/View/Widgets/LeaderboardList.dart
+++ my_app/lib/App/View/Widgets/LeaderboardList.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 
 class Leaderboardlist extends StatelessWidget {
 // Demo data for demonstration
