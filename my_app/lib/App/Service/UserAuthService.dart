836fe4e632a23c57267c3b54c35248d5e2cb75da
Mahtab Nihar <77922821+mahtabnihar@users.noreply.github.com>
2024-12-14 04:01:49 +0600

    Delete my_app directory
    finishing priv

--- my_app/lib/App/Service/UserAuthService.dart
+++
@@ -1,8 +0,0 @@
-import 'package:get/get.dart';
-
-class Userauthservice extends GetxService {
-  var token = "".obs;
-  void setAuthData(Map<String, dynamic> data) {
-    token.value = data['token'] ?? "";
-  }
-}
